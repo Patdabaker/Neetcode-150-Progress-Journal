746a908c7a8c0a5269f5e55073eaf0acea1593e1
Kristian Bell <kristianbell1999@gmail.com>
2025-06-09 20:52:22 -0700

    Added Contains Duplicate solution

---
+++ Arrays_and_Hashing/Contains_Duplicate.py
@@ -0,0 +1,15 @@
+"""
+Problem: Contains Duplicate
+Link: https://neetcode.io/problems/duplicate-integer?list=neetcode150
+Category: Arrays & Hashing
+
+Approach:
+- Use a dictionary to store frequency values
+- For each value, check if greater than 1 to show duplicates and return True, else False
+"""
+class Solution:
+    def hasDuplicate(self, nums):
+        numsdict = {}
+        for num in nums:
+            numsdict[num] = numsdict.get(num, 0) + 1
+        return any(value > 1 for value in numsdict.values())
